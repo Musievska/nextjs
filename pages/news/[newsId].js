ed2c631b514e6a63a4062b47f2e58b36884de5f2
Dilyana Musievska <dilyana.musievska@gmail.com>
2022-05-26 13:32:51 +0300

    exploring

---
+++ pages/news/[newsId].js
@@ -0,0 +1,4 @@
+//square brac. [] in the file name is fow a dynamic page
+//useRoutes is built in next hook
+
+import {useRouter } from 'next/router';
---
+++ pages/news/index.js
@@ -0,0 +1,9 @@
+//our-domain.com/news
+//folders in pages also act as path segments!!!
+//our-domein.com/news/something important => we need to create a subfolder and then create a new file which is subling to index.js in folder news
+
+function NewsPage() {
+    return <h1>News Page</h1>
+}
+
+export default NewsPage;

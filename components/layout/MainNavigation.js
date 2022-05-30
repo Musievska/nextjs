9a587059a7f1e5d080ac0a60b1224697cb1d3e10
Dilyana Musievska <dilyana.musievska@gmail.com>
2022-05-30 14:26:47 +0300

    added details comp

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,5 +1,6 @@
 import classes from './MainNavigation.module.css';
 import Link from 'next/link';
+//https://nextjs.org/docs/api-reference/next/link
 
 function MainNavigation() {
 
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupList from '../../components/meetups/MeetupList';
+import { Fragment } from 'react';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+
+function MeetupDetails() {
+    return (
+    <MeetupDetail>
+
+        </MeetupDetail>
+    )
+}
+
+export default MeetupDetails;

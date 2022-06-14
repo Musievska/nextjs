1de87b66b92df3bfe37524300d9648176c2a95a3
Dilyana Musievska <dilyana.musievska@gmail.com>
2022-06-14 16:15:09 +0300

    meetup details page added css

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,20 +1,16 @@
-import { Fragment } from 'react';
+import classes from './MeetupDetails.module.css';
 
 function MeetupDetail(props) {
     return (
-        <Fragment>
-            <img src={props.image}
-                alt={props.title}/>
-            <h1>
-                {props.title}
-            </h1>
-            <address>
-                {props.address}
-            </address>
-            <p>
-                {props.description}
-            </p>
-        </Fragment>
+        <section className={classes.detail}>
+            <img
+                src={props.image}
+                alt={props.title}
+            />
+            <h1>{props.title}</h1>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+        </section>
     );
 }
 
--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -2,11 +2,26 @@
 // /api/new-meetup
 // POST/api/new-meetup => to be sure only post requests will trigure
 
+import { MongoClient } from 'mongodb';
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
-        
+        const client = await MongoClient.connect(
+            ``
+        );
+
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({ message: 'Meetup inserted!' });
     }
 }
 
